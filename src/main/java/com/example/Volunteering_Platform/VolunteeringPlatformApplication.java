0dad9cb28dab0414fccf9a14ca3c63579d5070c0
Ajeet <choubeyajeet0@gmail.com>
2025-02-18 15:24:25 +0530

    Initialized the project

---
+++ src/main/java/com/example/Volunteering_Platform/VolunteeringPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.Volunteering_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteeringPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteeringPlatformApplication.class, args);
+	}
+
+}
